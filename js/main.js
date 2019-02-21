6ce74592cba6b457d1120d230b3909833d5ff50a
Hugo Pineda <hpineda83@gmail.com>
2019-02-20 23:10:00 -0800

    Use tiles

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
 (function() {
+  function makeField(numRows, numCols) {
+    var fieldContainer = document.getElementById("fieldContainer");
+    var colWidth = 16;
+
+    fieldContainer.style.width = String(numCols * colWidth) + "px";
+
+    for (var i = 0; i < numRows; i++) {
+      for (var j = 0; j < numCols; j++) {
+        var cell = document.createElement("div");
+        cell.className = "cell";
+        fieldContainer.appendChild(cell);
+
+        // cell.addEventListener("click", function() {
+        //   handleCellClick(this, i, j);
+        // });
+      }
+    }
+  }
+
   function handleCellClick(elem, i, j) {
     elem.className += " pressed";
     console.log(i, j);
   }
 
-  var fieldContainer = document.getElementById("fieldContainer");
-  var colWidth = 20;
-  var numRows = 10;
-  var numCols = 10;
-
-  fieldContainer.style.width = String(numCols * colWidth) + "px";
-
-  for (var i = 0; i < numRows; i++) {
-    for (var j = 0; j < numCols; j++) {
-      var cell = document.createElement("div");
-      var inner = document.createElement("div");
-      cell.className = "cell";
-      inner.className = "inner";
-      cell.appendChild(inner);
-      fieldContainer.appendChild(cell);
-
-      cell.addEventListener("click", function() {
-        handleCellClick(this, i, j);
-      });
-    }
-  }
+  makeField(20, 20);
 })();
