e5098be7739473ab0225b8d83a64e52d8bc7372b
Hugo Pineda <hpineda83@gmail.com>
2019-03-02 14:10:09 -0800

    more styling

--- js/main.js
+++ js/main.js
@@ -25,10 +25,25 @@
     fieldContainer.addEventListener("click", function(ev) {
       var cell = ev.target;
       console.log("click on", cell.dataset.i, cell.dataset.j);
+      cell.className = "cell pressed-0";
+    });
+
+    fieldContainer.addEventListener("contextmenu", function(ev) {
+      ev.preventDefault();
+      var cell = ev.target;
+      console.log("right click on", cell.dataset.i, cell.dataset.j);
     });
   }
 
   var numRows = 8;
   var numCols = 8;
   makeField(numRows, numCols);
+
+  var smileyButton = document.getElementById("smiley-button");
+  smileyButton.addEventListener("mousedown", function(ev) {
+    smileyButton.className = "panel smiley down";
+  });
+  smileyButton.addEventListener("mouseup", function(ev) {
+    smileyButton.className = "panel smiley";
+  });
 })();
